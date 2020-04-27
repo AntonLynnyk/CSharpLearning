51b6131eaa488ebe76791792da41518600a17e53
Anton Lynnyk <antonlynnyk@gmail.com>
2020-04-27 17:52:41 +0300

    First commit of source code

---
+++ TestProgram/TestProgram/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string: ");
+
+            string phrase = Console.ReadLine();
+
+            string[] words = phrase.Split(' ');
+
+            int[] numbers = ConvAndAssign(words);
+
+            int maxValue = ExplorMaxValue(numbers);
+
+            Console.WriteLine($"Max value of array is {maxValue}");
+        }
+
+        static int[] ConvAndAssign(string[] words)
+        {
+            int[] numbers = new int[words.Length];
+
+            int number;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                number = Int32.Parse(words[i]);
+
+                numbers[i] = number;
+            }
+            return numbers;
+        }
+
+        static int ExplorMaxValue(int[] values)
+        {
+            int maxValue = values[0];
+
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] > maxValue)
+                {
+                    maxValue = values[i];
+                }
+            }
+            return maxValue;
+        }
+    }
+}
