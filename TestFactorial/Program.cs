9bc95ea020eeacfa9421b8d230eba7f1bd37c9cd
Serhii Harashchenko <device11194@gmail.com>
2020-05-12 20:56:15 +0300

    Adding new method for calculation factorial and fixing some issues.

--- TestFactorial/Program.cs
+++ TestFactorial/Program.cs
@@ -12,24 +12,49 @@ namespace TestFactorial
         {
             Console.WriteLine("Enter number of factorial: ");
 
-            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
 
-            int factorial = FindingFactorial(number);
+                int factorial = FindingFactorialWithLoop(number);
 
-            Console.WriteLine($"The factorial of {number} is {factorial}");
+                Console.WriteLine($"The factorial of {number} is {factorial}");
+            }
+            catch (System.FormatException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
 
-            Console.ReadLine();
+                Console.WriteLine("You should enter only numbers, not strings!");
+
+                int number1 = int.Parse(Console.ReadLine());
+
+                int factorial1 = FindingFactorial(number1);
+
+                Console.WriteLine($"The factorial of {number1} is {factorial1}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
         static int FindingFactorial(int value)
         {
-            if(value == 1)
+            if (value == 1)
             {
-                return value;
+                return 1;
             }
             else
             {
                 return value = value * FindingFactorial(value - 1);
             }
+        }
+        static int FindingFactorialWithLoop(int value)
+        {
+            for (int i = value; i > 1; i--)
+            {
+                    value *= (i - 1);
+            }
+            return value;
         }
     }
 }
