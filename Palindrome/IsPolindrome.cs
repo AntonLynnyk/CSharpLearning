ca26283e431b239ba700e164936ce5ee19f3be0a
Serhii Harashchenko <device11194@gmail.com>
2020-06-16 21:36:18 +0300

    Program which determine that the entered word is palindrome or not.

---
+++ Palindrome/IsPolindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public static class IsPolindrome
+    {
+        public static int DeterminePolindrome(char[] arrayLetters)
+        {
+            int k = 0;
+
+            for (int i = arrayLetters.Length - 1; i >= 0; i--)
+            {
+                if (arrayLetters[k] == arrayLetters[i])
+                {
+                    k++;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+
+        
+
+            
+    }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("With this app you can determine that your entered word is polindrome or not");
+
+            Console.WriteLine("Enter you word:");
+
+            string word = Console.ReadLine();
+
+            char[] arrayLetters = word.ToCharArray();
+
+            int i = IsPolindrome.DeterminePolindrome(arrayLetters);
+
+            if (i == 1)
+            {
+                Console.WriteLine("Your word is a polindrome");
+            }
+            else
+            {
+                Console.WriteLine("Your word is not a polindrome");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
