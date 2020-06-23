4e63c0d6f7e5d804d88b2e1b8da6fa597e4755bd
Serhii Harashchenko <device11194@gmail.com>
2020-06-24 00:18:51 +0300

    UnitTests

---
+++ UnitTests/IsPolindromeTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class IsPolindromeTests
+    {
+        [TestMethod]
+        public void DeterminePolindrome_CorrectPolindrome_ReturnsTrue() 
+        {
+            // SetUp 
+            string correctPolindrome = "123454321";
+
+            // Action
+            bool result = IsPolindrome.DeterminePolindrome(correctPolindrome);
+
+            // Check
+            MyAssert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void DeterminePolindrome_IncorrectPolindrome_ReturnsFalse() 
+        {
+            // SetUp 
+            string incorrectPolindrome = "123451234";
+
+            // Action
+            bool result = IsPolindrome.DeterminePolindrome(incorrectPolindrome);
+
+            // Check
+            MyAssert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void DeterminePolindrome_EmptyString_ReturnsFalse() 
+        {
+            string emptyString = "";
+
+            bool result = IsPolindrome.DeterminePolindrome(emptyString);
+
+            MyAssert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void DeterminePolindrome_SingleCharacter_ReturnsTrue() 
+        {
+            string singleCharacter = "1";
+
+            bool result = IsPolindrome.DeterminePolindrome(singleCharacter);
+
+            MyAssert.IsTrue(result);
+        }
+
+        //public void DeterminePolindrome_StringIncludeSpace_ReturnsFalse() { }
+
+        public void DeterminePolindrome_CorrectPolindromeIncludesSymbols_ReturnsTrue() { }
+    }
+}
---
+++ UnitTests/MyAssert.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    class MyAssert
+    {
+        /// <summary>
+        /// Checks the condition and throws the exeption if condition is false.
+        /// </summary>
+        /// <param name="condition">Condition that is expected to be false</param>
+        public static void IsTrue (bool condition)
+        {
+            if (condition != true)
+            {
+                throw new Exception("Assert is false but expected true");
+            }
+        }
+        
+        /// <summary>
+        /// Checks the condition and throws the exeption if condition is true.
+        /// </summary>
+        /// <param name="condition">Condition that is expected to be true</param>
+        public static void IsFalse (bool condition)
+        {
+            if (condition != false)
+            {
+                throw new Exception("Assert is true but expected false");
+            }
+        }
+    }
+}
